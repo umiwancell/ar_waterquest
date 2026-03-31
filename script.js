7abb4b7e28a27169475bea3520f68a840b0c834b
umiwancell <kumiwan0823@gmail.com>
2026-03-31 14:30:34 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+let tappedCount = 0;
+
+AFRAME.registerComponent('clickable', {
+  init: function () {
+    this.el.addEventListener('click', () => {
+
+      if (this.el.getAttribute("visible") === false) return;
+
+      this.el.setAttribute("visible", false);
+      tappedCount++;
+
+      if (tappedCount === 3) {
+        document.querySelector("#infoCard")
+          .setAttribute("visible", true);
+      }
+    });
+  }
+});
+
+document.getElementById("resetBtn").addEventListener("click", () => {
+
+  tappedCount = 0;
+
+  document.querySelectorAll(".animal").forEach(el => {
+    el.setAttribute("visible", true);
+  });
+
+  document.querySelector("#infoCard")
+    .setAttribute("visible", false);
+});
